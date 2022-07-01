d99fbb0863ee5b94a007c4e3188df7b2ef07440e
gghotted <gghotted2@naver.com>
2022-07-01 20:23:00 +0900

    container

---
+++ README.md
@@ -0,0 +1,7 @@
+# fluid vs fixed
+
+fluid: 창 크기에 비례하여 스무스 하게 변함
+
+fixed: 창 크기에 비례하여 끊겨서(각지게) 변함
+
+[영상](https://likepython.tistory.com/23)
--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div>app</div>
+    <div>
+      <Container sx={{backgroundColor: 'red'}}>
+        <Typography color={"primary"} maxWidth={'md'}>md fluid</Typography>
+      </Container>
+      <Container sx={{backgroundColor: 'blue'}} fixed>
+        <Typography color={"primary"} maxWidth={'md'}>md fixed</Typography>
+      </Container>
+    </div>
   );
 }
 
