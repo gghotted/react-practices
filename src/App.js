019b7a3aa7a18169dcf50469139fa49b671e46b9
gghotted <gghotted2@naver.com>
2022-07-02 15:37:52 +0900

    basic navigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
+import { Button } from '@mui/material';
+
+function One() {
+  const navigate = useNavigate();
+  return (
+    <Button
+      onClick={() => {navigate('/two')}}
+    >
+      Two로 가기
+    </Button>
+  )
+}
+
+function Two() {
+  return (
+    <div>
+      two
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div>app</div>
+    <Router>
+      <Routes>
+        <Route path="/one" element={<One/>}></Route>
+        <Route path="/two" element={<Two/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
