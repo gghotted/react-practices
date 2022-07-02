260bc337f71e7e6a48d83ff274a360b3b60365c5
gghotted <gghotted2@naver.com>
2022-07-02 13:52:48 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams
+} from 'react-router-dom';
+
+function Detail() {
+  let { id } = useParams();
+  return <div>detail {id}</div>
+}
 
 function App() {
   return (
-    <div>app</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>root</div>}></Route>
+        <Route path="/create" element={<div>create</div>}></Route>
+        <Route path="/:id" element={<Detail/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
