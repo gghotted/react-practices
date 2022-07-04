1242daebb43004db37fa0c4d1c4001bd2d76bfda
gghotted <gghotted2@naver.com>
2022-07-04 23:29:38 +0900

    table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(id, name, category) {
+  return {id, name, category};
+}
+
+const rows = [
+  createData(1, '애쉬', '원딜'),
+  createData(2, '다리우스', '전사'),
+  createData(3, '룰루', '서포터'),
+  createData(4, '신드라', '마법사'),
+  createData(5, '에코', '마법사'),
+]
 
 function App() {
   return (
-    <div>app</div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>타입</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow>
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.category}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
